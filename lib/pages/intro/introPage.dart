7d036f677f836e6c3ff97212529ae48e44446e88
paulin Mpolesha <97231577+Pathel7@users.noreply.github.com>
2024-04-19 14:34:48 +0100

    on working

--- lib/pages/intro/introPage.dart
+++ lib/pages/intro/introPage.dart
@@ -5,12 +5,35 @@ class IntroPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Scaffold(
-      body: Column(
-        children: [
-          
-          Center(child: Text("Bienvenue sur EvenTask")),
-        ],
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: Column(
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: 20.0,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(30.0)),
+                  boxShadow: [
+                    BoxShadow(color: Colors.grey.shade200, offset: Offset(0, 8))
+                  ]),
+              height: 130.0,
+              width: 200.0,
+              child: Padding(
+                padding: const EdgeInsets.all(18.0),
+                child: Image.asset(
+                  "assets/images/logo1.png",
+                ),
+              ),
+            ),
+            
+          ],
+        ),
       ),
     ));
   }
